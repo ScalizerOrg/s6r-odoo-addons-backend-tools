19174e3e3f12a7563d5364abd911ffc16e53b051
superColson <colin.chibois@gmail.com>
2025-01-29 15:33:59 +0100

    [FIX] import odoo.tests only in test mode

--- s6r_unit_tests/tests/__init__.py
+++ s6r_unit_tests/tests/__init__.py
@@ -1,6 +1,9 @@
 # Copyright 2023 Scalizer (<https://www.scalizer.fr>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from . import runner
-from . import loader
-from . import common
+# in v18+, trying to import odoo.tests when test_enable is False will print a logger.error
+from odoo.tools import config
+if config['test_enable']:
+    from . import runner
+    from . import loader
+    from . import common
