2ee565cac33d80e437759df478c62b0adfd8ba36
Michel Perrocheau <michel@scalizer.fr>
2025-09-15 15:25:36 +0200

    [FIX] avoid "Importing test framework" warning on database creation warning message : 'avoid importing from business modules and when not running in test mode'

--- s6r_unit_tests/__init__.py
+++ s6r_unit_tests/__init__.py
@@ -2,5 +2,6 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from . import models
-from . import tests
-
+from odoo.tools.config import config
+if config['test_enable'] or config['test_file']:
+    from . import tests
