82ed2b9d020a3a9f4679adb608e8e08a996d6529
Vincent Barrat <vincent@scalizer.fr>
2025-12-15 17:04:11 +0100

    Migrate to Odoo 19.0
    - Update version from 18.0.1.0.0 to 19.0.1.0.0
    - Fix import: OdooTestResult moved from odoo.tests.loader to odoo.tests.result

--- s6r_unit_tests/tests/runner.py
+++ s6r_unit_tests/tests/runner.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Scalizer (<https://www.scalizer.fr>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo.tests.loader import OdooTestResult
+from odoo.tests.result import OdooTestResult
 import logging
 
 _logger = logging.getLogger(__name__)
