7619b434ba91989cb0909b39dab3dafaefc37257
Khalid BENTALEB <khalid@scalizer.fr>
2026-03-27 15:56:51 +0100

    [FIX] Remove mail_bot_hr module from white list

--- s6r_unit_tests/tests/test_patch_invisible_fields.py
+++ s6r_unit_tests/tests/test_patch_invisible_fields.py
@@ -267,7 +267,6 @@ def patched_test_uncommented_invisible_field(self):
         'loyalty',
         'lunch',
         'mail',
-        'mail_bot_hr',
         'mail_group',
         'maintenance',
         'maintenance_worksheet',
